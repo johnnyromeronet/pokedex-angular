7ff0cf3911d844212385dbb2e8c00a652ae4e7ed
Johnny Romero <jonathanromerosantos@gmail.com>
2025-10-04 14:04:49 +0200

    Limpieza

--- src/app/pokedex-main/pokedex-main.ts
+++ src/app/pokedex-main/pokedex-main.ts
@@ -1,11 +1,10 @@
-import { Component, Inject, PLATFORM_ID } from '@angular/core';
+import { Component } from '@angular/core';
 import { PokedexScreen } from '../pokedex-screen/pokedex-screen';
 import { PokedexControl } from '../pokedex-control/pokedex-control';
 import { PokedexEvolution } from '../pokedex-evolution/pokedex-evolution';
 import { PokedexDetail } from '../pokedex-detail/pokedex-detail';
 import { PokedexWeakness } from '../pokedex-weakness/pokedex-weakness';
 import { Pokemon } from '@data/pokemon.model';
-import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-pokedex-main',
@@ -55,35 +54,6 @@ export class PokedexMain {
   ];
 
   selectedPokemon: Pokemon = this.data[0];
-
-  constructor(
-    @Inject(PLATFORM_ID) private readonly _platformId: Object
-  ) {
-    debugger;
-    if (isPlatformBrowser(this._platformId)) {
-      // Busca un elemento
-      const found = this.data.find(p => p.id === 2);
-      console.log(found);
-
-      // Filtra elementos
-      const filtered = this.data.filter(p => p.name.includes('saur'));
-      console.log(filtered);
-
-      // Transforma elementos
-      const names = this.data.map(p => p.name.toUpperCase());
-      console.log(names);
-
-      // Ordena elementos
-      const ordered = this.data.sort((a, b) => a.name.localeCompare(b.name));
-      console.log(ordered);
-
-      localStorage.setItem('pokemons', JSON.stringify(this.data));
-      const saved = localStorage.getItem('pokemons');
-      if (saved) {
-        const data = JSON.parse(saved);
-      }
-    }
-  }
 
   selectPokemon(pokemon: Pokemon) {
     this.selectedPokemon = pokemon;
